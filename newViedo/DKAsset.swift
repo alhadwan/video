c7a3198d2fa139f635a03e61e08d792ee7a51c33
Ali Hadwan <alihadwan@Alis-MacBook-Pro.local>
2016-03-29 17:45:23 -0700

    first commit

---
+++ newViedo/DKAsset.swift
@@ -0,0 +1,13 @@
+//
+//  DKAsset.swift
+//  newViedo
+//
+//  Created by Ali Hadwan on 3/26/16.
+//  Copyright © 2016 Ali Hadwan. All rights reserved.
+//
+
+import UIKit
+
+class DKAsset: NSObject {
+
+}
